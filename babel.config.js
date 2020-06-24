5c8f3835e863068539ff2cdfdce0140cdab3f1d7
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 19:04:29 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,60 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        useBuiltIns: 'entry',
-        targets: {
-          node: 'current',
-          browsers: [
-            'last 2 versions'
-          ]
-        },
-        corejs: 3
-      }
-    ],
-    '@babel/react'
+const debug = require('debug')
+
+const log = debug('shinkansen')
+
+const {
+  env: {
+    NODE_ENV = 'development'
+  }
+} = process
+
+log('`shinkansen` is awake')
+
+function env () {
+  log({ NODE_ENV })
+
+  return (
+    NODE_ENV === 'production'
+  )
+}
+
+const presets = [
+  [
+    '@babel/env', {
+      useBuiltIns: 'entry',
+      targets: {
+        node: 'current',
+        browsers: [
+          'last 2 versions'
+        ]
+      },
+      corejs: 3
+    }
   ],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    '@babel/proposal-class-properties',
-    [
-      'module-resolver', {
-        alias: {
-          'shinkansen-gears': './src',
-          build: './build',
-          stories: './stories'
-        }
+  '@babel/react'
+]
+
+const plugins = [
+  '@babel/proposal-export-default-from',
+  '@babel/proposal-class-properties',
+  [
+    'module-resolver', {
+      alias: {
+        'shinkansen-gears': './src',
+        build: './build',
+        stories: './stories'
       }
-    ]
+    }
   ]
+]
+
+module.exports = (api) => {
+  if (api) api.cache.using(env)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
